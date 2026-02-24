1e4e903b4914649293e0be49b4e9391b11ac2631
ArunYuvaraj <arunyuvaraj.sde@gmail.com>
2026-02-24 19:41:39 +0530

    Initial commit: Springboot order placement project

---
+++ src/test/java/com/nexware/orderplacement/OrderplacementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexware.orderplacement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderplacementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
